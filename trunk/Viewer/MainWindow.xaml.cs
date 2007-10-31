4b1bc2210d83d5fc24b51586694e11ed620bff42
pavelsavara <pavelsavara@2614b305-bb3b-0410-9a76-c7ca2f9a0522>
2007-10-31 20:05:31 +0000

    animation details

--- trunk/Viewer/MainWindow.xaml.cs
+++ trunk/Viewer/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace Viewer
             cube.FrontModel.Transform = TrackBall.Transform;
             cube.BackModel.Transform = TrackBall.Transform;
             TrackBall.EventSource = CaptureBorder;
+            cube.OnBeforeAnimation += BeforeAnimation;
+            cube.OnAfterAnimation += AfterAnimation;
+            textBlockInfo.Text = cube.Cube.ToString();
         }
 
         private Trackball TrackBall = new Trackball();
@@ -30,7 +33,6 @@ namespace Viewer
             TrackBall.Reset();
         }
 
-
         private void buttonBottomReset_Click(object sender, RoutedEventArgs e)
         {
             TrackBall.Reset();
@@ -38,32 +40,65 @@ namespace Viewer
 
         private void buttonFlip_Click(object sender, RoutedEventArgs e)
         {
+            if (!buttonsEnabled) return;
             cube.Flip();
         }
 
         private void buttonTurn_Click(object sender, RoutedEventArgs e)
         {
+            if (!buttonsEnabled) return;
             cube.Turn();
         }
 
         private void buttonBottomRight_Click(object sender, RoutedEventArgs e)
         {
+            if (!buttonsEnabled) return;
             cube.RotateNextBot();
         }
 
         private void topTopRight_Click(object sender, RoutedEventArgs e)
         {
+            if (!buttonsEnabled) return;
             cube.RotateNextTop();
         }
 
         private void buttonBottomLeft_Click(object sender, RoutedEventArgs e)
         {
+            if (!buttonsEnabled) return;
             cube.RotatePrevBot();
         }
 
         private void buttonTopLeft_Click(object sender, RoutedEventArgs e)
         {
+            if (!buttonsEnabled) return;
             cube.RotatePrevTop();
+        }
+
+        private void AfterAnimation()
+        {
+            //RefreshButtons(true);
+            buttonsEnabled = true;
+            textBlockInfo.Text = cube.Cube.ToString();
+        }
+
+        private void BeforeAnimation()
+        {
+            //RefreshButtons(false);
+            buttonsEnabled = false;
+        }
+
+        private bool buttonsEnabled = true;
+
+        private void RefreshButtons(bool enabled)
+        {
+            buttonTopLeft.IsEnabled = enabled;
+            buttonTopReset.IsEnabled = enabled;
+            buttonTopRight.IsEnabled = enabled;
+            buttonBottomLeft.IsEnabled = enabled;
+            buttonBottomReset.IsEnabled = enabled;
+            buttonBottomRight.IsEnabled = enabled;
+            buttonFlip.IsEnabled = enabled;
+            buttonTurn.IsEnabled = enabled;
         }
     }
 }
