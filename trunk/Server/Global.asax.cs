6da7e8432ba49c1f8c0e9d8663730f275a465d3b
pavelsavara <pavelsavara@2614b305-bb3b-0410-9a76-c7ca2f9a0522>
2007-11-13 00:11:31 +0000

    ajax page

---
+++ trunk/Server/Global.asax.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Server.Properties;
+using Zamboch.Cube21.Work;
+
+namespace Server
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        private static DatabaseManager manager;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            if (manager == null)
+            {
+                manager = new DatabaseManager(Settings.Default.DatabasePath);
+                manager.Initialize();
+            }
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
